374f7d77016b6f8e118b92183cd2c0816f58683d
obsoletse <1191430716@qq.com>
2020-08-06 22:29:04 +0800

    项目新简历提交

---
+++ src/main/java/com/linbin/jmu/JmuApplication.java
@@ -0,0 +1,13 @@
+package com.linbin.jmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmuApplication.class, args);
+    }
+
+}
