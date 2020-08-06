a2cbc97d7dfc5122bec6db8768ea7332749cfed2
obsoletse <1191430716@qq.com>
2020-08-06 22:38:20 +0800

    配置文件和启动类修改

--- src/main/java/com/linbin/jmu/JmuApplication.java
+++ src/main/java/com/linbin/jmu/JmuApplication.java
@@ -1,13 +1,25 @@
 package com.linbin.jmu;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+
+@Slf4j //开启日志的注解
 @SpringBootApplication
 public class JmuApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JmuApplication.class, args);
+        ConfigurableApplicationContext application = SpringApplication.run(JmuApplication.class, args);
+        Environment env = application.getEnvironment();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application info is running! Access URLs:\n\t" +
+                "Local: http://localhost:" + port + path + "/\n\t" +
+                "----------------------------------------------------------");
     }
 
 }
